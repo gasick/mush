7ee86ff93fc0a6a3af0de67712a4b3bd4b5c6caf
gasick <gasick@mail.ru>
2020-01-09 04:01:38 +0300

    Запилил раздачу карт

--- src/CardDeck.kt
+++ src/CardDeck.kt
@@ -2,79 +2,92 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 
 class CardDeck {
-    val number = 36;
+    //собираем колоду карт
     var cardDeck: List<Card> = listOf(
-    Card(0, Suits.spades, Symbols.Ace),
-    Card(0, Suits.spades, Symbols.King) ,
-    Card(0, Suits.spades, Symbols.Queen),
-    Card(0, Suits.spades, Symbols.Jack) ,
-    Card(0, Suits.spades, Symbols.Ten)  ,
-    Card(0, Suits.spades, Symbols.Nine) ,
-    Card(0, Suits.spades, Symbols.Eight),
-    Card(0, Suits.spades, Symbols.Seven),
-    Card(0, Suits.spades, Symbols.Six)  ,
-    Card(0, Suits.hearts, Symbols.Ace)  ,
-    Card(0, Suits.hearts, Symbols.King) ,
-    Card(0, Suits.hearts, Symbols.Queen),
-    Card(0, Suits.hearts, Symbols.Jack) ,
-    Card(0, Suits.hearts, Symbols.Ten)  ,
-    Card(0, Suits.hearts, Symbols.Nine) ,
-    Card(0, Suits.hearts, Symbols.Eight),
-    Card(0, Suits.hearts, Symbols.Seven),
-    Card(0, Suits.hearts, Symbols.Six)  ,
-    Card(0, Suits.clubs, Symbols.Ace)   ,
-    Card(0, Suits.clubs, Symbols.King)  ,
-    Card(0, Suits.clubs, Symbols.Queen) ,
-    Card(0, Suits.clubs, Symbols.Jack)  ,
-    Card(0, Suits.clubs, Symbols.Ten)   ,
-    Card(0, Suits.clubs, Symbols.Nine)  ,
-    Card(0, Suits.clubs, Symbols.Eight) ,
-    Card(0, Suits.clubs, Symbols.Seven) ,
-    Card(0, Suits.clubs, Symbols.Six)   ,
-    Card(0, Suits.diamonds, Symbols.Ace),
-    Card(0, Suits.diamonds, Symbols.King),
-    Card(0, Suits.diamonds, Symbols.Queen),
-    Card(0, Suits.diamonds, Symbols.Jack),
-    Card(0, Suits.diamonds, Symbols.Ten),
-    Card(0, Suits.diamonds, Symbols.Nine),
-    Card(0, Suits.diamonds, Symbols.Eight),
-    Card(0, Suits.diamonds, Symbols.Seven),
-    Card(0, Suits.diamonds, Symbols.Six)
+    Card( Suits.spades, Symbols.Ace),
+    Card( Suits.spades, Symbols.King) ,
+    Card( Suits.spades, Symbols.Queen),
+    Card( Suits.spades, Symbols.Jack) ,
+    Card( Suits.spades, Symbols.Ten)  ,
+    Card( Suits.spades, Symbols.Nine) ,
+    Card( Suits.spades, Symbols.Eight),
+    Card( Suits.spades, Symbols.Seven),
+    Card( Suits.spades, Symbols.Six)  ,
+    Card( Suits.hearts, Symbols.Ace)  ,
+    Card( Suits.hearts, Symbols.King) ,
+    Card( Suits.hearts, Symbols.Queen),
+    Card( Suits.hearts, Symbols.Jack) ,
+    Card( Suits.hearts, Symbols.Ten)  ,
+    Card( Suits.hearts, Symbols.Nine) ,
+    Card( Suits.hearts, Symbols.Eight),
+    Card( Suits.hearts, Symbols.Seven),
+    Card( Suits.hearts, Symbols.Six)  ,
+    Card( Suits.clubs, Symbols.Ace)   ,
+    Card( Suits.clubs, Symbols.King)  ,
+    Card( Suits.clubs, Symbols.Queen) ,
+    Card( Suits.clubs, Symbols.Jack)  ,
+    Card( Suits.clubs, Symbols.Ten)   ,
+    Card( Suits.clubs, Symbols.Nine)  ,
+    Card( Suits.clubs, Symbols.Eight) ,
+    Card( Suits.clubs, Symbols.Seven) ,
+    Card( Suits.clubs, Symbols.Six)   ,
+    Card( Suits.diamonds, Symbols.Ace),
+    Card( Suits.diamonds, Symbols.King),
+    Card( Suits.diamonds, Symbols.Queen),
+    Card( Suits.diamonds, Symbols.Jack),
+    Card( Suits.diamonds, Symbols.Ten),
+    Card( Suits.diamonds, Symbols.Nine),
+    Card( Suits.diamonds, Symbols.Eight),
+    Card( Suits.diamonds, Symbols.Seven),
+    Card( Suits.diamonds, Symbols.Six)
     )
 
-    fun CardDeck(){
-        for (c: Card in cardDeck)  {
-            //TODO добавить генерацию случайного порядка карт c.numberInDeck
-        }
-    }
     fun printDeck() {
+
         fun takeCard(): Card {
-            val n = Random.nextInt(1..36)
+            //Вытаскиваем номер карты
+            val n = Random.nextInt(0..35)
+            //Если карта не использованна в игре то метим её как карту которая учавствовала в игре
             if (!cardDeck[n].inuse) cardDeck[n].inuse = true
+            //Если карта использовалась запускаем заново функция взятия карты
             else takeCard()
+            //Возвращаем карту запросившему
             return cardDeck[n]
         }
+
+        //Вызываем все карты по порядку из колоды
+        //чтобы смотрим состояние
         for (c: Card in cardDeck){
             //TODO вызвать рандомные карты из колоды.
-            print("${c.symbol.toString()} of ${c.suit.toString()} is in use: ${c.inuse};")
+            print("${c.symbol.toString()} of ${c.suit.toString()} is in use: ${c.inuse}; ")
         }
         println()
-        for ( x in 1..36){
-            //TODO не очень выходит сделать вывод случайной карты.
-            var card = takeCard()
-            print("${card};")
-            //println("${c.symbol.toString()} of ${c.suit.toString()}")
+
+        //раздаем карты 4 участникам
+        for (x in 1..4 ) {
+           println("Player $x have:")
+           for (y in 1..4){
+               var card = takeCard()
+               print("${card.symbol.toString()} of ${card.suit.toString()} is in use: ${card.inuse}; ")
+            }
+            println()
         }
+        //Выводим все карты чтобы знать. что карты которые учавствовали в игре помечены к сбросу.
         for (c: Card in cardDeck){
             //TODO вызвать рандомные карты из колоды.
-            print("${c.symbol.toString()} of ${c.suit.toString()} is in use: ${c.inuse};")
+            print("${c.symbol.toString()} of ${c.suit.toString()} is in use: ${c.inuse}; ")
         }
 
     }
 }
 
-class Card(n: Int,su: Suits,sy: Symbols) {
+
+//Создаем класс карта
+class Card(su: Suits,sy: Symbols) {
+    //Даем карте свойство "В игре"
     var inuse: Boolean = false
+    //Даем карте свойство масти
     val suit: Suits = su
+    //Даем карте свойство ранга
     val symbol: Symbols = sy
 }
